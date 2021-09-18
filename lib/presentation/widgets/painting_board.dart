0d66b36446dc8ba634c513df10c3627bf87665f0
Gabriel Alessio Vieira <relga44dev@gmail.com>
2021-09-18 23:34:55 +0200

    added support for background color

--- lib/presentation/widgets/painting_board.dart
+++ lib/presentation/widgets/painting_board.dart
@@ -6,8 +6,10 @@ class PaintingBoard extends StatelessWidget {
     Key? key,
     double? boardHeight,
     double? boardWidth,
+    Color? boardBackgroundColor,
   })  : _boardHeight = boardHeight ?? double.infinity,
         _boardWidth = boardWidth ?? double.infinity,
+        _boardBackgroundColor = boardBackgroundColor ?? Colors.grey,
         super(key: key);
 
   /// The height of the board.
@@ -22,9 +24,18 @@ class PaintingBoard extends StatelessWidget {
   /// horizontal space.
   final double _boardWidth;
 
+  /// The background color of the board.
+  ///
+  /// Note that if this property is set, [boardDecoration] must be null.
+  ///
+  /// If this value is not set and [boardDecoration] is not provided,
+  /// the default background color will be [Colors.grey].
+  final Color _boardBackgroundColor;
+
   @override
   Widget build(BuildContext context) => Container(
         height: _boardHeight,
         width: _boardWidth,
+        color: _boardBackgroundColor,
       );
 }
