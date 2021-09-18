1c1faf99da36affb8fc7dac4042d1e56cae079ba
Gabriel Alessio Vieira <relga44dev@gmail.com>
2021-09-18 23:47:38 +0200

    created example

---
+++ example/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_painting_tools/presentation/widgets/painting_board.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: PaintingBoard(),
+        ),
+      ),
+    );
+  }
+}
