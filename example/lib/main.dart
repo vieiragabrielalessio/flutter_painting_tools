83d57d4e16361a93ffefe627a17f6b65f80e45ae
Gabriel Alessio Vieira <relga44dev@gmail.com>
2021-09-19 17:52:51 +0200

    updated example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_painting_tools/presentation/widgets/painting_board.dart';
+import 'package:flutter_painting_tools/flutter_painting_tools.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +12,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: PaintingBoard(
-            boardHeight: 500,
-            boardWidth: 300,
-          ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late final PaintingBoardController controller;
+
+  @override
+  void initState() {
+    controller = PaintingBoardController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: PaintingBoard(
+          boardHeight: 500,
+          boardWidth: 300,
+          controller: controller,
         ),
       ),
     );
